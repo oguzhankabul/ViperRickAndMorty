083d63a37b7ad0349eee478e83344564be4771c2
oguzhan.kabul <oguzhan@vivoo.me>
2022-04-04 03:04:28 +0300

    settings

---
+++ ViperRickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViperRickAndMorty
+//
+//  Created by Oğuzhan Kabul on 4.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
